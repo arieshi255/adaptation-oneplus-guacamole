09e651c12c8a5b09d2a22ce13d09eb4b2350b9a7
Rushi <44952533+Skyliegirl33@users.noreply.github.com>
2021-11-06 18:05:55 +0100

    Script to build adaptation zip

---
+++ package.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+tar --exclude='./debian' --exclude='./.git' --exclude='./.github' --exclude='./META-INF' --exclude='./package.sh' -cvf payload.tar .
+
+zip -r9 adaptation-test.zip META-INF/ payload.tar 
