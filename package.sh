260f18ae02088a45c2abdcfd121ad3715ecbd5ba
Rushi <44952533+Skyliegirl33@users.noreply.github.com>
2021-11-07 02:00:48 +0000

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-tar --exclude='./debian' --exclude='./.git' --exclude='./.github' --exclude='./META-INF' --exclude='./kernel' --exclude='./package.sh' -cvf payload.tar .
+tar --exclude='./debian' --exclude='./.git' --exclude='./.github' --exclude='./META-INF' --exclude='./kernel' --exclude='./package.sh' --exclude='./boot.img' -cvf payload.tar .
 
-zip -r9 adaptation-test.zip META-INF/ payload.tar 
+zip -r9 adaptation-test.zip META-INF/ boot.img payload.tar 
